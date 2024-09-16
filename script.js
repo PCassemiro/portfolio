a44f3bfcba9fdd3b459f9f7e4a36a95a6ed1182b
PCassemiro <pedrobr2909@gmail.com>
2024-09-16 16:50:35 -0300

    Adição de JS

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var img= window.document.getElementById('imagem')
-var imagem= img.value;
+//Função de ao clicar na imagem mudar a mesma
+var IdImagem=window.document.getElementById('imagem')
 function clicar() {
+    IdImagem.set
+    IdImagem.setAttribute('src', 'pixels.jpg')
 }
-//colocar pra quando clicar na imagem a mesma mudar 
+//
